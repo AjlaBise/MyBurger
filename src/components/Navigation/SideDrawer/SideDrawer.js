11a73ef56e7f9f0c4572d5767e4ef63cc8af84cb
ajlabise <57880454+ajlabise@users.noreply.github.com>
2020-11-27 19:54:46 +0100

    AddingASidedrawerToggleButton

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,29 +6,26 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Auxiliary.js';
 
 const SideDrawer = (props) => {
- 
-    let attachedClasses =[classes.SideDrawer, classes.Close];
-    if(props.open)
-    {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
-return (
-  <Aux>
-      <Backdrop show={props.open} clicked={props.closed}/>
-    <div className={attachedClasses.join(' ')}>
-        <div className={classes.Logo}>
-        <Logo/>
-        
-        </div>
-        
-        <nav>
-            <NavigationItems/>
-        </nav>
-    </div>
-    </Aux>
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
 
-    
-);
+
+    );
 };
 
 export default SideDrawer
