d92b6327af799e835b1f0e5c28233fc171ec2d07
ajlabise <57880454+ajlabise@users.noreply.github.com>
2020-11-27 21:43:03 +0100

    ChangingTheFolderStructure

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.css';
-import DrawerToggle from '../SideDrawer/DrawerToggle/DraweToggle';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const Toolbar = (props) => (
     <header className={classes.Toolbar}>
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import classes from './Model.css';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 
